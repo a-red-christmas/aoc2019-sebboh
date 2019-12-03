13a927eecd34692b24433d21fda4aca0bc279c9c
sebboh <david@the-good-guys.net>
2019-12-03 15:29:37 -0600

    make it work on guix

--- day/1/first.sh
+++ day/1/first.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [[ -r $1 ]] 
 then n=0; for i in `cat $1`; do echo $((n+=$(( $i / 3 - 2)))); done |tail -1
 else echo I require an argument.  see \`cat first.sh\` for usage.
